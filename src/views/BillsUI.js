7fe569a3434a420a17cdfdfca38b520dfefc82b8
Startom78 <tom78630@gmail.com>
2025-05-23 16:25:24 +0200

    fix sort dates

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -1,31 +1,38 @@
-import VerticalLayout from './VerticalLayout.js'
-import ErrorPage from "./ErrorPage.js"
-import LoadingPage from "./LoadingPage.js"
+import VerticalLayout from "./VerticalLayout.js";
+import ErrorPage from "./ErrorPage.js";
+import LoadingPage from "./LoadingPage.js";
 
-import Actions from './Actions.js'
-  
+import Actions from "./Actions.js";
+import { formatDate } from "../app/format.js";
+
 const row = (bill) => {
-  return (`
+    return `
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
-      <td>${bill.date}</td>
+      <td data-testid="${bill.date}">${formatDate(bill.date)}</td>
       <td>${bill.amount} €</td>
       <td>${bill.status}</td>
       <td>
         ${Actions(bill.fileUrl)}
       </td>
     </tr>
-    `)
-  }
+    `;
+};
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
-}
+    if (!data || !data.length) {
+        return "";
+    }
+    const sorted = [...data].sort((a, b) => {
+        return new Date(b.date) - new Date(a.date);
+    });
+    console.log("je suis la " + sorted.map((a) => a.date));
+    return sorted.map((bill) => row(bill)).join("");
+};
 
 export default ({ data: bills, loading, error }) => {
-  
-  const modal = () => (`
+    const modal = () => `
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
@@ -40,15 +47,15 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
     </div>
-  `)
+  `;
 
-  if (loading) {
-    return LoadingPage()
-  } else if (error) {
-    return ErrorPage(error)
-  }
-  
-  return (`
+    if (loading) {
+        return LoadingPage();
+    } else if (error) {
+        return ErrorPage(error);
+    }
+
+    return `
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -75,6 +82,5 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
       ${modal()}
-    </div>`
-  )
-}
+    </div>`;
+};
